1f1b200d0dea0efb87fbfb94d876711821c778e7
prem <premenaga@gmail.com>
2021-10-24 00:20:04 +0530

    h

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import { Provider } from 'react-redux';
 import './App.css';
+import CakeContainer from './components/cakeContainer';
+import HooksCakeContainer from './components/HooksCakeConatiner';
+import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import store from './Redux/Store';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store} >
+      <div className="App">
+        {/* <HooksCakeContainer />
+     <IceCreamContainer />
+     <NewCakeContainer />
+     <CakeContainer />
+     <ItemContainer  cake/> */}
+        
+        <UserContainer />
+      </div>
+    </Provider>
   );
 }
 
